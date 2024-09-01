e8968f896238d1c7ca27ea28b932b16a706ef125
euniiiii <hee9920@naver.com>
2024-09-01 12:42:48 +0900

    :sparkles: initial setting

---
+++ src/main/java/com/team/thundergather/ThundergatherApplication.java
@@ -0,0 +1,13 @@
+package com.team.thundergather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThundergatherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThundergatherApplication.class, args);
+    }
+
+}
