5f8fbe3621dc185bb36c30f5380db50c26b1577b
euniiiii <hee9920@naver.com>
2024-09-01 12:55:58 +0900

    :pushpin: webconfig 추가

---
+++ src/main/java/com/team/thundergather/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.team.thundergather.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+
+                // Frontend
+                .allowedOrigins("http://localhost:3000",
+                        "https://main--beo-gather.netlify.app"
+                )
+
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
