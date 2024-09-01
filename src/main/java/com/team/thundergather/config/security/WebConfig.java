153536235c32fcf33baf4c788c36badcc5276acc
euniiiii <hee9920@naver.com>
2024-09-01 13:22:22 +0900

    :recycle: 폴더 구조 변경 - webconfig를 security 폴더 아래로 이동

--- src/main/java/com/team/thundergather/config/security/WebConfig.java
+++ src/main/java/com/team/thundergather/config/security/WebConfig.java
@@ -1,4 +1,4 @@
-package com.team.thundergather.config;
+package com.team.thundergather.config.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
